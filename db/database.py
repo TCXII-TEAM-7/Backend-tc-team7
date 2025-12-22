babce3f9e0507f3b3d580ab9d5966d454d8b8a96
oussama <admaneo99@gmail.com>
2025-12-23 00:00:30 +0100

    hosting the db

--- db/database.py
+++ db/database.py
@@ -2,8 +2,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+# "postgresql://postgres:pg_for_test@localhost:5432/doxa_callcenter"
 SQLALCHEMY_DATABASE_URL = (
-    "postgresql://postgres:pg_for_test@localhost:5432/doxa_callcenter"
+    "postgresql://tcxii_user:w9OQanO9T1N8Sa5sEPpKS93rElXMb98y@dpg-d54splshg0os739o11p0-a.virginia-postgres.render.com/tcxii"
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
